9c5989a3fe2143dfe6a182450567d6647634693f
Erick Oliveira <erick.oliveira9607@gmail.com>
2024-03-29 21:14:53 -0300

    first commit

--- frontweb/src/pages/Admin/Employees/Form/index.tsx
+++ frontweb/src/pages/Admin/Employees/Form/index.tsx
@@ -1,9 +1,52 @@
 import './styles.css';
+import { Employee } from 'types/employee';
+import { Department } from 'types/department';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import Select from 'react-select';
+
+import { requestBackend } from 'util/requests';
+import { useHistory } from 'react-router-dom';
+import { AxiosRequestConfig } from 'axios';
+import { toast } from 'react-toastify';
 
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm<Employee>();
+  const history = useHistory();
+
+  const [selectDepartments, setSelectDepartments] = useState<Department[]>([]);
+
+  useEffect(() => {
+    requestBackend({ withCredentials: true, url: '/departments' }).then(
+      (response) => {
+        setSelectDepartments(response.data);
+      }
+    );
+  }, []);
+
+  const onSubmit = (formData: Employee) => {
+    const config: AxiosRequestConfig = {
+      method: 'POST',
+      url: '/employees',
+      data: formData,
+      withCredentials: true,
+    };
+    requestBackend(config)
+      .then(() => {
+        toast.info('Cadastrado com sucesso');
+        history.push('/admin/employees');
+      })
+      .catch(() => toast.error('Erro ao cadastrar o funcionário'));
+  };
 
   const handleCancel = () => {
-    // to do
+    history.push('/admin/employees');
   };
 
   return (
@@ -11,28 +54,74 @@ const Form = () => {
       <div className="base-card employee-crud-form-card">
         <h1 className="employee-crud-form-title">INFORME OS DADOS</h1>
 
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="row employee-crud-inputs-container">
             <div className="col employee-crud-inputs-left-container">
-
               <div className="margin-bottom-30">
-                <input type="text" 
-                  className="form-control base-input is-invalid"
+                <input
+                  {...register('name', {
+                    required: 'Campo obrigatório',
+                  })}
+                  name="name"
+                  data-testid="name"
+                  type="text"
+                  placeholder="Nome do funcionário"
+                  className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
                 />
                 <div className="invalid-feedback d-block">
-                  Mensagem de erro
+                  {errors.name?.message}
                 </div>
-              </div>
 
-              <div className="margin-bottom-30">
-                <input type="text" 
-                  className="form-control base-input"
+                <input
+                  {...register('email', {
+                    required: 'Campo obrigatório',
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: 'Email inválido',
+                    },
+                  })}
+                  name="email"
+                  data-testid="email"
+                  type="text"
+                  placeholder="Email do funcionário"
+                  className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
                 />
                 <div className="invalid-feedback d-block">
-                  
+                  {errors.email?.message}
+                </div>
+
+                <div className="margin-bottom-30 ">
+                  <Controller
+                    name="department"
+                    rules={{ required: true }}
+                    control={control}
+                    render={({ field }) => (
+                      <Select
+                        {...field}
+                        placeholder="Departamento"
+                        aria-label="Departamento"
+                        options={selectDepartments}
+                        classNamePrefix="product-crud-select"
+                        getOptionLabel={(department: Department) =>
+                          department.name
+                        }
+                        getOptionValue={(department: Department) =>
+                          String(department.id)
+                        }
+                      />
+                    )}
+                  />
+                  {errors.department && (
+                    <div className="invalid-feedback d-block">
+                      Campo obrigatório
+                    </div>
+                  )}
                 </div>
               </div>
-
             </div>
           </div>
           <div className="employee-crud-buttons-container">
--- frontweb/src/pages/Admin/Employees/List/index.tsx
+++ frontweb/src/pages/Admin/Employees/List/index.tsx
@@ -3,18 +3,22 @@ import './styles.css';
 import Pagination from 'components/Pagination';
 import EmployeeCard from 'components/EmployeeCard';
 import { Link } from 'react-router-dom';
-
-const employeeHardCode = { // delete
-  id: 1,
-  name: "Carlos",
-  email: "carlos@gmail.com",
-  department: {
-    id: 1,
-    name: "Sales"
-  }
-};
+import { useEffect, useState } from 'react';
+import { requestBackend } from 'util/requests';
+import { Employee } from 'types/employee';
+import { SpringPage } from 'types/vendor/spring';
+import { hasAnyRoles } from 'util/auth';
 
 const List = () => {
+  const [employees, setEmployees] = useState<SpringPage<Employee>>();
+
+  useEffect(() => {
+    requestBackend({ url: '/employees', withCredentials: true }).then(
+      (response) => {
+        setEmployees(response.data);
+      }
+    );
+  }, []);
 
   const handlePageChange = (pageNumber: number) => {
     // to do
@@ -22,16 +26,17 @@ const List = () => {
 
   return (
     <>
-      <Link to="/admin/employees/create">
-        <button className="btn btn-primary text-white btn-crud-add">
-          ADICIONAR
-        </button>
-      </Link>
+      {hasAnyRoles(['ROLE_ADMIN']) && (
+        <Link to="/admin/employees/create">
+          <button className="btn btn-primary text-white btn-crud-add">
+            ADICIONAR
+          </button>
+        </Link>
+      )}
 
-      <EmployeeCard employee={employeeHardCode} />
-      <EmployeeCard employee={employeeHardCode} />
-      <EmployeeCard employee={employeeHardCode} />
-      <EmployeeCard employee={employeeHardCode} />
+      {employees?.content.map((employee) => (
+        <EmployeeCard employee={employee} key={employee.id} />
+      ))}
 
       <Pagination
         forcePage={0}
